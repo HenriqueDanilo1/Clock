d48f32526a4248839925fe35c54c5830b4e187cf
HenriqueDanilo1 <danilohenolive@gmail.com>
2024-06-20 16:53:25 -0300

    finished yeeeeee

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let time = document.getElementById('time')
+let h = document.getElementById('h')
+let m = document.getElementById('m')
+let s = document.getElementById('s')
 function gettime(){
     let now = new Date()
     let hour = now.getHours().toString().padStart(2, "0")
     let min = now.getMinutes().toString().padStart(2, "0")
     let sec = now.getSeconds().toString().padStart(2, "0")
-    time.innerHTML=`${hour} : ${min} : ${sec}`
+    h.innerHTML=`${hour} : `
+    m.innerHTML=`${min} : `
+    s.innerHTML=`${sec}`
 }
 gettime()
 
