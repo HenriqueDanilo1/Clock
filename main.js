da40ca79ac5ade5603f30b27680d97e2b162e02a
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-16 11:08:33 -0300

    lol

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 let h = document.getElementById('h')
 let m = document.getElementById('m')
 let s = document.getElementById('s')
-function gettime(){
-    let now = new Date()
-    let hour = now.getHours().toString().padStart(2, "0")
-    let min = now.getMinutes().toString().padStart(2, "0")
-    let sec = now.getSeconds().toString().padStart(2, "0")
-    h.innerHTML=`${hour} : `
-    m.innerHTML=`${min} : `
-    s.innerHTML=`${sec}`
-}
-gettime()
-
-setInterval(gettime, 1000)
+setInterval(() => {
+        let now  = new Date()
+        h.innerHTML = `${now.getHours().toString().padStart(2, "0")} : `
+        m.innerHTML = `${now.getMinutes().toString().padStart(2, "0")} : `
+        m.innerHTML = `${now.getSeconds().toString().padStart(2, "0")}`
+        let min  = now.getMinutes().toString().padStart(2, "0")
+        let sec  = now.getSeconds().toString().padStart(2, "0")
+        
+    }, 1000
+)
