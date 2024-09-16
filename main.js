103c66e30c2af680de6f930665a7361601ef0734
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-16 11:09:22 -0300

    lol

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ setInterval(() => {
         let now  = new Date()
         h.innerHTML = `${now.getHours().toString().padStart(2, "0")} : `
         m.innerHTML = `${now.getMinutes().toString().padStart(2, "0")} : `
-        m.innerHTML = `${now.getSeconds().toString().padStart(2, "0")}`
+        s.innerHTML = `${now.getSeconds().toString().padStart(2, "0")}`
         let min  = now.getMinutes().toString().padStart(2, "0")
         let sec  = now.getSeconds().toString().padStart(2, "0")
         
