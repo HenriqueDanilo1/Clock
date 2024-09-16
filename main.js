ed14ba66bef26410d5826448469969db7642bfa8
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-16 11:12:29 -0300

    lol

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let h = document.getElementById('h')
 let m = document.getElementById('m')
 let s = document.getElementById('s')
 //1000ms error fixed
+let now  = new Date()
 h.innerHTML = `${now.getHours().toString().padStart(2, "0")} : `
 m.innerHTML = `${now.getMinutes().toString().padStart(2, "0")} : `
 s.innerHTML = `${now.getSeconds().toString().padStart(2, "0")}`
