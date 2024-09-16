b3cb91dcd62b7938ecfde24294868b11eec83224
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-16 11:11:00 -0300

    os cara ri com limao

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//dom declaration
 let h = document.getElementById('h')
 let m = document.getElementById('m')
 let s = document.getElementById('s')
+//1000ms error fixed
+h.innerHTML = `${now.getHours().toString().padStart(2, "0")} : `
+m.innerHTML = `${now.getMinutes().toString().padStart(2, "0")} : `
+s.innerHTML = `${now.getSeconds().toString().padStart(2, "0")}`
+//clocks brain
 setInterval(() => {
         let now  = new Date()
         h.innerHTML = `${now.getHours().toString().padStart(2, "0")} : `
